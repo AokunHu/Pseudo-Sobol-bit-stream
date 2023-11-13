586a40285d95267affb3c1ab0fe2ec0ba15c88a7
AokunHu <115856587+AokunHu@users.noreply.github.com>
2023-11-13 14:35:02 +0800

    Update sobol_comp.m

--- matlab/sobol_comp.m
+++ matlab/sobol_comp.m
@@ -1,10 +1,3 @@
-% @File    :   sobol_comp.m
-% @Time    :   2021/9/10
-% @Author  :   Dongxu Lv 
-% @Version :   0.1
-% @Contact :   lvdongxu@sjtu.edu.cn
-% @License :   (C)Copyright 2020-forever , SJTU-DMNE
-% @Desc    :   Sobol sequence generator based on comparator
 
 function seq = sobol_comp(x, len, src_num)
 % Input:  
